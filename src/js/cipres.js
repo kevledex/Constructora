867ac93c38dd87f0880a4018e91010c968fa1f5f
kevledex <kevinledesma014@gmail.com>
2026-01-29 17:52:56 -0500

    Agregando la pagina reserva, y validando si esta vendido el inmueble

--- src/js/cipres.js
+++ src/js/cipres.js
@@ -8,8 +8,20 @@ btn.addEventListener('mouseenter', () => {
     btn.style.color = "white";
 });
 
-// Evento cuando el cursor sale del botón
 btn.addEventListener('mouseleave', () => {
     btn.style.backgroundColor = "white";
     btn.style.color = "black";
 });
+
+//Boton volver arriba
+const botonirArriba = document.getElementById("btn-volver-arriba");
+
+window.onscroll = () => {
+    if (window.scrollY > 700) {
+        botonirArriba.classList.remove("translate-y-20", "opacity-0");
+        botonirArriba.classList.add("translate-y-0", "opacity-100");
+    } else {
+        botonirArriba.classList.add("translate-y-20", "opacity-0");
+        botonirArriba.classList.remove("translate-y-0", "opacity-100");
+    }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
 import '../css/style.css'; 
 import 'flowbite';
+
+//Boton volver arriba
+const botonirArriba = document.getElementById("btn-volver-arriba");
+
+window.onscroll = () => {
+    if (window.scrollY > 700) {
+        botonirArriba.classList.remove("translate-y-20", "opacity-0");
+        botonirArriba.classList.add("translate-y-0", "opacity-100");
+    } else {
+        botonirArriba.classList.add("translate-y-20", "opacity-0");
+        botonirArriba.classList.remove("translate-y-0", "opacity-100");
+    }
+};
